213607b59d9797b30732a35e055d9fb28bb48d85
Shores Forgotten <shores@shoresforgotten.xyz>
2025-03-04 18:14:05 +0100

    Split Archipelago library off into its own repo

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.1.0
+
+- Split library off from Archipelabutt
---
+++ test/archipelago_dart_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:archipelago_dart_2/archipelago_dart_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
